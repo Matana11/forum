06d5f27ac5e18e3fb30922fd97fe7f1854f061bb
matana <matanacm@163.com>
2020-01-19 16:29:55 +0800

    add index and bootstrap

--- src/main/java/forum/kaoyan/qinghuagong/controller/IndexController.java
+++ src/main/java/forum/kaoyan/qinghuagong/controller/IndexController.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 //注意首字母大写
 //加注解后，就会自动识别扫描当前类，把它当做spring的一个并局管理，同时也识别它为controller，意思就是允许这个类
 //接收前端的请求。
+//一个反斜杠代表根目录，代表任何东西都不输入的时候，默认访问index
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";//写完这句再去templates下写hello.html
-    }
+public class IndexController {
+    @GetMapping("/")
+    public String index(){return "index";}
 }
