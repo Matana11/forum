5031c7d4bb2c7027ebddf5db77b05abd4ecc6179
matana <matanacm@163.com>
2020-02-12 16:40:44 +0800

    add database

--- src/main/java/forum/kaoyan/qinghuagong/controller/AuthorizeController.java
+++ src/main/java/forum/kaoyan/qinghuagong/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package forum.kaoyan.qinghuagong.controller;
 
 import forum.kaoyan.qinghuagong.dto.AccessTokenDTO;
 import forum.kaoyan.qinghuagong.dto.GithubUser;
+import forum.kaoyan.qinghuagong.mapper.UserMapper;
+import forum.kaoyan.qinghuagong.model.User;
 import forum.kaoyan.qinghuagong.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,12 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 //写一个方法，返回string因为要返回到一个页面去
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;//直接在这里面定义，然后写一个注解，就会自动把刚才实例化好的东西放里面。
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -25,7 +33,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         //怎么样接收参数呢，就是requestParameter
         //requestparameter不知道什么参数就摁ctrl+p,看提示
         //state需要接收吗，文档中下文调access_token时，还有一个state，这个state陈述说是random string，第一步提供的。所以要再填一个参数
@@ -36,9 +45,24 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);//光标选中new AccessTokenDTO()，快捷键ctrl+alt+v，把变量快速提取到外面
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);//token用UUID设置
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));//id是long类型的，accountId是string类型，强制转换String.valueof
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写cookies和session
+            request.getSession().setAttribute("user",githubUser);//把user对象放到session里面
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
 
-        return "index";  //登录成功之后，返回到index页面
+
     }
 }
---
+++ src/main/java/forum/kaoyan/qinghuagong/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package forum.kaoyan.qinghuagong.mapper;
+
+import forum.kaoyan.qinghuagong.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
