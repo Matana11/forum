eabc699b510f66cd8b8b8ef52274429c7e440485
matana <matanacm@163.com>
2020-02-13 22:07:05 +0800

    add check cookies

--- src/main/java/forum/kaoyan/qinghuagong/controller/AuthorizeController.java
+++ src/main/java/forum/kaoyan/qinghuagong/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 //写一个方法，返回string因为要返回到一个页面去
@@ -21,7 +22,7 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;//直接在这里面定义，然后写一个注解，就会自动把刚才实例化好的东西放里面。
 
-    @Autowired
+    @Autowired(required = false)
     private UserMapper userMapper;
 
     @Value("${github.client.id}")
@@ -34,7 +35,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         //怎么样接收参数呢，就是requestParameter
         //requestparameter不知道什么参数就摁ctrl+p,看提示
         //state需要接收吗，文档中下文调access_token时，还有一个state，这个state陈述说是random string，第一步提供的。所以要再填一个参数
@@ -55,8 +56,8 @@ public class AuthorizeController {
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登录成功，写cookies和session
-            request.getSession().setAttribute("user",githubUser);//把user对象放到session里面
+            response.addCookie(new Cookie("token",token));
+           //
             return "redirect:/";
         }else{
             //登录失败，重新登录
--- src/main/java/forum/kaoyan/qinghuagong/controller/IndexController.java
+++ src/main/java/forum/kaoyan/qinghuagong/controller/IndexController.java
@@ -1,9 +1,16 @@
 package forum.kaoyan.qinghuagong.controller;
 
+import forum.kaoyan.qinghuagong.mapper.UserMapper;
+import forum.kaoyan.qinghuagong.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Handler;
 
 //注意首字母大写
 //加注解后，就会自动识别扫描当前类，把它当做spring的一个并局管理，同时也识别它为controller，意思就是允许这个类
@@ -11,6 +18,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 //一个反斜杠代表根目录，代表任何东西都不输入的时候，默认访问index
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){return "index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
+        return "index";
+
+    }
 }
